8ebe00b2f880102bb0d61ab2382075655e342c20
Dime Bomou <bomoudime5@gmail.com>
2026-02-18 23:04:59 +0000

    saisir un entier negatif

--- projet git.c
+++ projet git.c
@@ -2,23 +2,27 @@
 
 
 int main(){
-int n ,x ,somme=0,cpt=0;
-float moy
+int n ,x ,i,somme=0,cpt=0,cpt2=0;//le compteur de nombre pair//
+float moyenne
 do{
     printf("saisir un entier");
     scanf("%d",&n);
 }while(n<0);
 for(i=0;i<n;i++){
     do{
-        puts("veuillez saisir un entier negatif");
+        puts("saisir un entier negatif");
         scanf("%d",&x);
 
     }while(x>0);
     if(x%2==0){
         somme=somme+x;
         cpt++;
+    }else{
+    cpt2++;
     }
 }
-moy=somme/cpt;
+moyenne=(float)somme/cpt;
+printf("\n la moyenne est de : 1.%f\n", moyenne);
+printf("le nombre de nombre impair est: %d",cpt2)
 }
 
